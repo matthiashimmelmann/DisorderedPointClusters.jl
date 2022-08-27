ecbaa221f424749f4fd1812cc60422469ce6781f
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2022-08-27 13:10:25 +0200

    Update DisorderedPointClusters.jl

--- src/DisorderedPointClusters.jl
+++ src/DisorderedPointClusters.jl
@@ -3,14 +3,13 @@ module DisorderedPointClusters
 import GLMakie: scatter!, Axis3, Figure, Point3f
 import LinearAlgebra: norm
 import HomotopyContinuation: Variable, Expression, evaluate, differentiate
-export generateDisorderedPointClusters
-    #TODO add analytic differentiation
 
+export generateDisorderedPointClusters
     function insertNecks(layers, curLayer, NGrid, Necks)
       pos = rand(1:NGrid^2)
       layerAbove = layers[curLayer==length(layers) ? 1 : curLayer+1][pos] == layers[curLayer][pos]
       layerBelow = layers[curLayer==1 ? length(layers) : curLayer-1][pos] == layers[curLayer][pos]
-      #At the moment only orthogonal adjacency asked for
+      #TODO at the moment only orthogonal adjacency.
       anyNeckAdjacent = any(t-> t==(curLayer%2==1 ? 2 : 1), layers[curLayer][vcat(pos, pos%NGrid==0 ? Int((pos/NGrid-1)*NGrid)+1 : pos+1, (pos-1)%NGrid==0 ? Int(((pos-1)/NGrid+1)*NGrid) : (pos-1), pos-NGrid<=0 ? pos-NGrid+NGrid^2 : pos-NGrid, pos+NGrid>NGrid^2 ? pos+NGrid-NGrid^2 : pos+NGrid)])
       if anyNeckAdjacent || layerAbove || layerBelow
         println("Insertion did not work, trying again...")
@@ -28,7 +27,6 @@ export generateDisorderedPointClusters
         xs[i,j,2] = Variable(:x, i, j, 2)
         xs[i,:,3] .= (0:1/NLayers:1-1/NLayers)[i]
       end
-      #TODO add NeckCableEquations
 
       for cable in NeckCables
         xs[cable[1]==NLayers ? 1 : cable[1]+1, cable[2], 1:2] = xs[cable[1], cable[2], 1:2]
@@ -36,43 +34,55 @@ export generateDisorderedPointClusters
       return(xs)
     end
 
-    function gradientdescent(gradient, HessQ, xvarz, curpoint)
-      cursol = Base.copy(curpoint)
-      while norm(evaluate(gradient, xvarz=>cursol))>1e-5
-        Jac = evaluate(HessQ, xvarz=>cursol)
-        cursol = cursol - Jac\evaluate(gradient, xvarz=>cursol)
-        #cursol = cursol - floor.(cursol)
-        display(norm(evaluate(gradient, xvarz=>cursol)))
+    function gradientDescent(initialPoint, periodic_xs, xvarz, σ1)
+      cursol = Base.copy(initialPoint)
+      global ∇Q, HessQ = energyFunction(periodic_xs, xvarz, σ1, cursol)
+      while norm(∇Q)>1e-5
+        cursol = cursol - HessQ\∇Q
+        cursol = cursol - floor.(cursol)
+        global ∇Q, HessQ = energyFunction(periodic_xs, xvarz, σ1, cursol)
+        display(norm(∇Q))
       end
       return(cursol)
     end
 
-    function simulateRepulsion(unitGrid, NLayers, NGrid, NeckCables, NNecks)
-      initialPoint = []
-      xvarz = []
-      for i in 1:NLayers
-        for j in 1:NGrid^2
-          if any(t->(t[1]==i-1||i==1&&t[1]==NLayers) && t[2]==j,NeckCables) || i==1 && (NLayers,j) in NeckCables
-            continue
-          else
-            append!(initialPoint, unitGrid[(i-1)*NGrid^2+j][1:2])
-            append!(xvarz, [Variable(:x, i, j, 1), Variable(:x, i, j, 2)])
-          end
+    function energyFunction(periodic_xs, xvarz, σ1, pt)
+      eval_periodic_xs = evaluate.(periodic_xs, xvarz=>pt)
+      list_of_relevant_molecule_interactions = []
+      for layer in 1:size(eval_periodic_xs)[1], i in Int(4*size(periodic_xs)[2]/9)+1:Int(5*size(periodic_xs)[2]/9), j in 1:size(periodic_xs)[2]
+        if sqrt(sum((eval_periodic_xs[layer,i,:]-eval_periodic_xs[layer,j,:]).^2)) < 2^(1/6)*sqrt(σ1) && i!=j && all(t->t[1]!=layer||t[2]!=j||t[3]!=i, list_of_relevant_molecule_interactions)
+          push!(list_of_relevant_molecule_interactions, (layer,i,j))
         end
       end
-      xvarz = Vector{Variable}(xvarz)
-      xs = generateMatrix(NLayers, NGrid, NNecks, NeckCables)
-
-      dist = vcat([(sum((xs[layer,i,:] - xs[layer,j,:]).^2)) for layer in 1:NLayers for i in 1:NGrid^2 for j in i+1:NGrid^2]...)
-      distBoundary = vcat([sum([(xs[layer,i,1]-0)^2,(xs[layer,i,1]-1)^2,(xs[layer,i,2]-0)^2,(xs[layer,i,2]-1)^2]) for layer in 1:NLayers for i in 1:NGrid^2]...)
-      NeckEquations = [sum((xs[cable[1],cable[2],:]-xs[cable[1],cable[3],:]).^2) for cable in filter(t->t[2]!=t[3], [vcat(cable,j) for cable in NeckCables for j in 1:NGrid^2])]
-      σ1 = 1/(1*NGrid^2)
-      σ2 = 1/(2*NGrid^2)
-      #TODO add a higher/lower factor for necks.
-      lennardJones = 4*sum((σ1/d)^6-(σ1/d)^3 for d in dist) + 4*sum((σ2/d)^6-(σ2/d)^3 for d in distBoundary) + 0.02*sum((1.8*σ1/d)^6-(1.8*σ1/d)^3 for d in NeckEquations)
+      distances = [sum((periodic_xs[layer,i,:] - periodic_xs[layer,j,:]).^2) for (layer,i,j) in list_of_relevant_molecule_interactions]
+      lennardJones = 4*sum((σ1/d)^6-(σ1/d)^3 for d in distances) #+ 4*sum((σ2/d)^6-(σ2/d)^3 for d in distBoundary) + 0.03*sum((1.5*σ1/d)^6-(1.5*σ1/d)^3 for d in NeckEquations)
       ∇Q = differentiate(lennardJones, xvarz)
-      HessQ = differentiate(differentiate(lennardJones, xvarz), xvarz)
-      result = gradientdescent(∇Q, HessQ, xvarz, initialPoint)
+      HessQ = differentiate(∇Q, xvarz)
+      return evaluate(∇Q, xvarz=>pt), evaluate(HessQ, xvarz=>pt)
+    end
+
+    function simulateRepulsion(unitGrid, NLayers, NGrid, NeckCables, NNecks)
+      initialPoint = Vector{Float64}([])
+      xvarz = Vector{Variable}([])
+      for i in 1:NLayers, j in 1:NGrid^2
+        if !any(t->(t[1]==i-1 || (i==1&&t[1]==NLayers)) && t[2]==j, NeckCables)
+          append!(initialPoint, unitGrid[(i-1)*NGrid^2+j][1:2])
+          append!(xvarz, [Variable(:x, i, j, 1), Variable(:x, i, j, 2)])
+        end
+      end
+
+      xs = generateMatrix(NLayers, NGrid, NNecks, NeckCables)
+      periodic_xs = Array{Expression,3}(undef, size(xs)[1], size(xs)[2]*9, 3)
+      #left to right, bottom to top 1-9
+      for i in 1:9
+        periodic_xs[:,(i-1)*NGrid^2+1:i*NGrid^2, :] = xs
+        periodic_xs[:,(i-1)*NGrid^2+1:i*NGrid^2, 1] .+= (i-1)%3-1
+        periodic_xs[:,(i-1)*NGrid^2+1:i*NGrid^2, 2] .+= Int(floor((i-1)/3))-1
+      end
+
+      #TODO improve performance by only looking at close neighbors
+      σ1 = 1/(NGrid^2)
+      result = gradientDescent(initialPoint, periodic_xs, xvarz, σ1)
 
       return(evaluate.(xs, xvarz=>result))
     end
@@ -106,6 +116,6 @@ export generateDisorderedPointClusters
     end
 
 
-    generateDisorderedPointClusters(6, 10, 6)
+    generateDisorderedPointClusters(6, 8, 6)
 
 end
