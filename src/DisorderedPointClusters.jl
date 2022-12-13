0df9069b412b09435fac62b46f51dc4d1791d7f5
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2022-12-13 11:12:17 +0100

    Unified into`DisorderedPointClusters.jl`
    + The three packages `GridWithNecks`1-3 have been unified in the main package `DisorderedPointClusters.jl`

--- src/DisorderedPointClusters.jl
+++ src/DisorderedPointClusters.jl
@@ -1,175 +1,178 @@
 module DisorderedPointClusters
+#Interactions are taken per 3 consecutive 2D-layers
 
 import GLMakie: scatter!, Axis3, Figure, Point3f0, Scene, cam3d!
 import LinearAlgebra: norm, det, pinv, svd
 import HomotopyContinuation: Variable, Expression, evaluate, differentiate
 import Random: shuffle
+import Combinatorics: multiexponents
 
-export generateDisorderedPointClusters
+export generateGridLayers
 
-#TODO disordered surface with >=3 labyrinths
-#TODO calculate one optimum. Transform the points upward in some way (e.g. rotation/reflection). Reflection is easy.
-#For rotation, multiply by e^ipi/2 = cos(pi/2)+isin(pi/2) with standard multiplication in C.
-#TODO 3D optimization on the Torus...
-#TODO put a repulsive grid around the necks
-#TODO For Layers: for each neck: look for nearby points and color them differently=> list 1:gridlength for each layer, delete in top/bottom layer and nearby (<2^(1/6)*sigma_1) and continue insertion.
+function createStar(neckCenter, neckSize, NGrid)
+    directions = collect(Set(vcat([[[part[1],part[2]], [-part[1],part[2]], [part[1],-part[2]], [-part[1],-part[2]]] for part in vcat([collect(multiexponents(2, exp)) for exp in 0:neckSize]...)]...)))
+    star = [[mod(((dir .+ neckCenter)[1]-1),NGrid)+1, mod(((dir .+ neckCenter)[2]-1),NGrid)+1] for dir in directions]
+    return star
+end
 
-    # Generate the 2D matrix corresponding to the particles in 1 layer
-    function generateMatrix(NGrid, NNecks, layerValue, totalLayers, numFixed)
-      xs=Array{Expression,2}(undef, NGrid+2*NNecks, 3)
-      for j in 1:NGrid+2*NNecks-numFixed
-        xs[j,1] = Variable(:x, j, 1)
-        xs[j,2] = Variable(:x, j, 2)
-      end
-      xs[:,3] .= layerValue
-      return(xs)
-    end
-
-    #Optimization method containing the calculation of the equations and a damped Newton's method.
-    function gradientDescent(initialPoint, periodic_xs, xvarz, σ1)
-      cursol = Base.copy(initialPoint)
-      eval_periodic_xs = evaluate.(periodic_xs, xvarz=>cursol)
-      list_of_relevant_molecule_interactions = []
-
-      for i in Int(4*size(periodic_xs)[1]/9)+1:Int(5*size(periodic_xs)[1]/9), j in 1:size(periodic_xs)[1]
-        if i!=j && all(t->t[1]!=j||t[2]!=i, list_of_relevant_molecule_interactions)
-          push!(list_of_relevant_molecule_interactions, (i,j))
-        end
-      end
-
-      distances = [sum((periodic_xs[i,:] - periodic_xs[j,:]).^2) for (i,j) in list_of_relevant_molecule_interactions]
-      #=lennardJones = sum(((σ1/d)^6-(σ1/d)^3 for d in distances)) #TODO repulsion/attraction between necks?
-      ∇Q = differentiate(lennardJones, xvarz)
-      HessQ = differentiate(∇Q, xvarz)=#
-      riesz = sum([1/d for d in distances])
-      ∇Q = differentiate(riesz, xvarz)
-      HessQ = differentiate(∇Q, xvarz)
-      savepoint = [randn(Float64) for _ in 1:length(cursol)]
-      index = 0
-      isMin = 0
-      while index < 10000
-        index = index+1;
-        if index%30 == 0
-          cursol = cursol - 1e-6*[rand(Float64) for _ in 1:length(cursol)]
-        end
-        cursol = cursol - 0.5*pinv(evaluate(HessQ, xvarz=>cursol))*evaluate(∇Q, xvarz=>cursol)
-        if any(t->t<0||t>1,cursol)
-          display("Left the Torus")
-          cursol = cursol - floor.(cursol)
-        end
-        display(norm(evaluate(∇Q, xvarz=>cursol)))
-        if norm(evaluate(∇Q, xvarz=>cursol))<=1e-6 && (all(t-> t>1e-6, svd(evaluate(HessQ, xvarz=>cursol)).S))
-          break;
-        elseif norm(evaluate(∇Q, xvarz=>cursol))<=1e-6 && isapprox(cursol,savepoint; atol=1e-3)
-          isMin = isMin + 1; 
-          if isMin == 2
-            break;
-          end
-        elseif norm(evaluate(∇Q, xvarz=>cursol))<=1e-6
-          isMin = 0
-          savepoint = cursol
-          cursol = cursol - 1e-6*[rand(Float64) for _ in 1:length(cursol)]
-          if any(t->t<0||t>1,cursol)
-            display("Left the Torus")
-            cursol = cursol - floor.(cursol)
-          end
-        end
-      end
-
-
-      return(cursol)
-    end
-
-    # This method creates (in total) 9 copies of the unit square the particles are submerged in to generate simulate interactions in a 2-torus
-    function simulateRepulsion(unitGrid, NGrid, NNecks, fixedPoints, curLayer, NLayers)
-      initialPoint = Vector{Float64}([])
-      xvarz = Vector{Variable}([])
-      previousLayer = []
-      layerList = (1:Int(length(unitGrid)/NLayers))[1:NGrid+2*NNecks-length(fixedPoints)]#shuffle(1:Int(length(unitGrid)/NLayers))[1:NGrid+2*NNecks-length(fixedPoints)]
-
-      for j in 1:NGrid+2*NNecks-length(fixedPoints)
-        append!(xvarz, [Variable(:x, j, 1), Variable(:x, j, 2)])
-        append!(initialPoint, unitGrid[Int((curLayer-1)*length(unitGrid)/NLayers)+layerList[j]][1:2])
-      end
-
-      xs = generateMatrix(NGrid, NNecks, (curLayer-1)/NLayers, NLayers, length(fixedPoints))
-      for i in 1:length(fixedPoints)
-        xs[i+NGrid+2*NNecks-length(fixedPoints), 1:2] = fixedPoints[i][1:2]
-      end
-      eval_xs = evaluate(xs, xvarz=>initialPoint)
-
-      periodic_xs = Array{Expression,2}(undef, size(xs)[1]*9, 3)
-      #NOTE left to right, bottom to top 1-9
-      for i in 1:9
-        periodic_xs[(i-1)*size(xs)[1]+1:i*size(xs)[1], :] = xs
-        periodic_xs[(i-1)*size(xs)[1]+1:i*size(xs)[1], 1] .+= (i-1)%3-1
-        periodic_xs[(i-1)*size(xs)[1]+1:i*size(xs)[1], 2] .+= Int(floor((i-1)/3))-1
-      end
-
-      σ1 = 1/((sqrt(2*NNecks+NGrid)))
-      result = gradientDescent(initialPoint, periodic_xs, xvarz, σ1)
-
-      return(evaluate.(xs, xvarz=>result))
-    end
-
-    # All points that are close enough are returned
-    function calculateAdjacents(pos, grid)
-      σ = 2^(1/6)/sqrt(size(grid)[1])
-      indices = findall(t->
-        any(q->norm(t-grid[pos,:]+q)<σ, [[a%3-1, floor((a-1)/3)-1, 0.] for a in 1:9]),
-        [grid[i,:] for i in 1:size(grid)[1]])
-      return indices
-    end
-
-    # Main Method
-    function generateDisorderedPointClusters(NGrid::Int, NNecks::Int, NLayers::Int)
-      NLayers%2==0 || throw(error("There must be an even number of layers!"))
-      maxNumberPoints = NGrid+2*NNecks
-      nextGridNumber = ceil(sqrt(maxNumberPoints)) #Calculate the next highest perfect square's square root.
-      gridArray = []
-      unitGrid = [Point3f0(a,b,c) for c in 0:1/NLayers:(NLayers-1)/NLayers for a in 1/(2*nextGridNumber):1/nextGridNumber:1-1/(2*nextGridNumber) for b in 1/(2*nextGridNumber):1/nextGridNumber:1-1/(2*nextGridNumber)]#[vcat([Int(nextGridNumber^2*(layer-1)+1):Int(nextGridNumber^2*(layer-1)+totalNumberPoints) for layer in 1:NLayers]...)]
-
-      correctedGrid = simulateRepulsion(unitGrid, NGrid, NNecks, [], 1, NLayers)
-      for z in 0:1/NLayers:(NLayers-1)/NLayers
-        helperGrid = Array{Float64,2}(undef, size(correctedGrid)[1], size(correctedGrid)[2])
-        helperGrid .= correctedGrid
-        helperGrid[:,3] .= z
-        push!(gridArray, helperGrid)
-      end
-
-      scene = Scene()
-      cam3d!(scene)
-      #NOTE Now we insert the Necks
-      numbersInGrid = [[i for i in 1:size(gridArray[layer])[1]] for layer in 1:length(gridArray)]
-      layers = [[i%2 for _ in 1:length(numbersInGrid[i])] for i in 1:length(gridArray)]
-      lengthVector = [NNecks for _ in 1:length(gridArray)]
-      for _ in 1:30
-        try 
-          for i in 1:length(gridArray)
-            for _ in 1:lengthVector[i]
-              randPos = rand(numbersInGrid[i])
-              layers[i][randPos] = 1-layers[i][randPos]
-              numbersInGrid[i] = filter(t-> !(t in calculateAdjacents(randPos, gridArray[i])), numbersInGrid[i])
-              numbersInGrid[i!=length(gridArray) ? i+1 : 1] = filter(t-> t!=randPos, numbersInGrid[i!=length(gridArray) ? i+1 : 1])
-              numbersInGrid[i!=1 ? i-1 : length(gridArray)] = filter(t-> t!=randPos, numbersInGrid[i!=1 ? i-1 : length(gridArray)])
-              numbersInGrid[(i < length(gridArray)-1) ? i+2 : (i!=length(gridArray) ? 1 : 2)] = filter(t-> t!=randPos, numbersInGrid[(i!=length(gridArray) && i!=length(gridArray)-1) ? i+2 : (i!=length(gridArray) ? 1 : 2)])
-              numbersInGrid[(i > 2) ? i-2 : (i!=1 ? length(gridArray) : length(gridArray)-1)] = filter(t-> t!=randPos, numbersInGrid[(i > 2) ? i-2 : (i!=1 ? length(gridArray) : length(gridArray)-1)])
+function color_Neck_Positions(neckConfig, xvarz, xs, totalGrid, layerColours, neckSize; MD_Method)
+    xs_eval = MD_Method=="2D-3" ? Array{Float64,3}(undef, size(layerColours)[1], Int(size(xs)[2]/3), 3) : Array{Float64,3}(undef, size(layerColours)[1], size(xs)[2], 3)
+    display(xs)
+    xs_eval[:,:,1:2] = MD_Method=="2D-3" ? evaluate.(xs, xvarz=>neckConfig)[:,1:Int(size(xs)[2]/3),1:2] : evaluate.(xs[:,:,1:2], xvarz=>neckConfig)
+    for layer in 1:size(layerColours)[1]
+        xs_eval[layer,:,3] .= (layer)/size(layerColours)[1]
+        for pos in 1:size(xs_eval)[2]
+            neckCenter = filter(t->isapprox(xs_eval[layer,pos,1:2], totalGrid[layer,t[1],t[2],1:2]; atol=1e-3), [(i,j) for i in 1:size(layerColours)[2] for j in 1:size(layerColours)[3]])[1]
+            indices = createStar(neckCenter, neckSize, size(layerColours)[2])
+            for index in indices
+                layerColours[layer, index[1], index[2]] = 1 .- layerColours[layer, index[1], index[2]]
             end
-          end
-          break;
-        catch e
-          numbersInGrid = [[i for i in 1:size(gridArray[layer])[1]] for layer in 1:length(gridArray)]
-          layers = [[i%2 for _ in 1:length(numbersInGrid[i])] for i in 1:length(gridArray)]    
-          continue;
         end
-      end   
-      #for posi in 1:9
-        foreach(k -> scatter!(scene, Point3f0(gridArray[k[2]][k[1],:]#=+[(posi-1)%3+1,floor((posi-1)/3),0]=#), color=layers[k[2]][k[1]] == 1 ? :red : :blue), [(i,j) for i in 1:size(gridArray[1])[1] for j in 1:length(gridArray)])
-      #end
-      display(scene)
-      foreach(k->println(gridArray[k[2]][k[1],1], " ", gridArray[k[2]][k[1],2], " ", gridArray[k[2]][k[1],3], " ", layers[k[2]][k[1]] == 1 ? "1" : "2"), [(i,j) for i in 1:size(gridArray[1])[1] for j in 1:length(gridArray)])
+    end
+    return(layerColours)
+end
+
+function create_list_of_relevant_distances(periodic_xs; MD_Method)
+  list_of_relevant_molecule_interactions = []
+  for layer1 in 1:size(periodic_xs)[1], layer2 in 1:size(periodic_xs)[1], pos_torus in Int(4*size(periodic_xs)[2]/9)+1:Int(5*size(periodic_xs)[2]/9), pos_general in 1:size(periodic_xs)[2]
+      if MD_Method == "2D-3" && (mod(pos_torus, Int(size(periodic_xs)[2]/9))!=mod(pos_general,Int(size(periodic_xs)[2]/9)) && all(t->(layer1!=t[1]||pos_torus!=t[4]||t[3]!=pos_general) && (layer1!=t[1]||pos_torus!=t[3]||t[4]!=pos_general), list_of_relevant_molecule_interactions))
+          push!(list_of_relevant_molecule_interactions, (layer1, layer1, pos_torus, pos_general))
+      elseif (MD_Method == "2D" || MD_Method == "3D")  && (mod(pos_torus, Int(size(periodic_xs)[2]/9))!=mod(pos_general,Int(size(periodic_xs)[2]/9)) || layer1!=layer2)
+          push!(list_of_relevant_molecule_interactions, (layer1, layer2, pos_torus, pos_general))
+      end
+  end
+
+  return [sum((periodic_xs[layer1,pos1,:] - periodic_xs[layer2,pos2,:]).^2) for (layer1, layer2, pos1, pos2) in list_of_relevant_molecule_interactions]
+end
+
+function create_Periods(NLayers, NNecks; MD_Method)
+    xs = MD_Method == "2D-3" ? Array{Expression,3}(undef, NLayers, NNecks*3, 2) : 
+           Array{Expression,3}(undef, NLayers, NNecks, MD_Method == "2D" ? 2 : 3)
+
+    for layer in 1:NLayers
+        xs[layer,1:NNecks,1:2] = [Variable(:x,layer,neck,xy) for xy in 1:2 for neck in 1:NNecks]
+        if MD_Method == "2D-3"
+          xs[(layer<NLayers ? layer+1 : 1),NNecks+1:2*NNecks,1:2] = [Variable(:x,layer,neck,xy) for xy in 1:2 for neck in 1:NNecks]
+          xs[(layer>1 ? layer-1 : NLayers),2*NNecks+1:3*NNecks,1:2] = [Variable(:x,layer,neck,xy) for xy in 1:2 for neck in 1:NNecks]
+        elseif MD_Method == "3D"
+          xs[layer,:,3] .= layer/NLayers
+        end
+    end
+    
+    periodic_xs = MD_Method == "3D" ? Array{Expression,3}(undef, NLayers, size(xs)[2]*9, 3) : Array{Expression,3}(undef, NLayers, size(xs)[2]*9, 2)
+    #NOTE left to right 1-2-3, bottom to top 1-4-7
+    for torus in 1:9
+        periodic_xs[:, (torus-1)*size(xs)[2]+1:torus*size(xs)[2], :] = xs
+        periodic_xs[:, (torus-1)*size(xs)[2]+1:torus*size(xs)[2], 1] .+= (torus-1)%3-1
+        periodic_xs[:, (torus-1)*size(xs)[2]+1:torus*size(xs)[2], 2] .+= Int(floor((torus-1)/3))-1
     end
 
+    return xs, periodic_xs
+end
 
-    generateDisorderedPointClusters(8, 4, 4)
+function gradientDescent(periodic_xs, initialPoint, variables, NGrid; energy, MD_Method, maxIter = 500)
+    #TODO take several random start points and do 1100 steps or so?
+    cursol = Base.copy(initialPoint)
+
+    distances = create_list_of_relevant_distances(periodic_xs; MD_Method = MD_Method)
+    Q=0;
+    if energy == "riesz"
+        Q = sum([1/d^2 for d in distances])
+    elseif energy == "lennardjones"
+        σ1 = MD_Method=="2D" ? sqrt(2)/(sqrt(size(periodic_xs)[1]*size(periodic_xs)[2]/9)) : sqrt(2)/(sqrt(size(periodic_xs)[2]/9))
+        Q = sum(((σ1/d)^6-(σ1/d)^3 for d in distances))
+    end
+    ∇Q = differentiate(Q, variables)
+    HessQ = differentiate(∇Q, variables)
+    isNoMin = 0
+    α = 0.5
+    prevsol = cursol
+    solutionarray = []
+
+    for iter in 1:maxIter
+        println(iter, " ", isNoMin," ", α, " ", norm(evaluate(∇Q, variables=>cursol)), " ", evaluate(Q, variables=>cursol))
+        stepdirection = pinv(evaluate.(HessQ, variables=>cursol))*evaluate(∇Q, variables=>cursol)
+        cursol = prevsol - α*stepdirection
+        α = iter%80 == 0 ? 0.5 : α
+        cursol = iter%530 == 0 ? mod.(Int.(round.(NGrid.*cursol)), NGrid) ./ NGrid .+ 1/(2*NGrid) : cursol
+        cursol = cursol - floor.(cursol)
+
+        if norm(evaluate(∇Q, variables => cursol)) > norm(evaluate(∇Q, variables => prevsol))
+            α = α/3
+        else
+            α = 1.1*α
+            α>0.5 ? α = 0.5 : nothing
+        end
+        #cursol = cursol + 1e-8*(rand(Float64, length(cursol)) .- 0.5)
+        cursol = cursol - floor.(cursol)
+
+
+        if norm(evaluate(∇Q, variables=>cursol)) <= 1e-2
+            isNoMin += 1;
+            push!(solutionarray, cursol)
+            cursol = cursol - 5e-1*(rand(Float64, length(cursol)) .- 0.5)
+            cursol = cursol - floor.(cursol)
+        end
+
+        prevsol = cursol
+        iter%5==0 && push!(solutionarray, cursol)
+    end
+    cursol = solutionarray[argmin([evaluate(Q, variables=>sol) for sol in solutionarray])]
+    #Round only in the end to grid
+    cursol = mod.(Int.(round.(NGrid.*cursol)), NGrid) ./ NGrid .+ 1/(2*NGrid)
+    cursol = cursol - floor.(cursol)
+
+    return cursol
+end
+
+#=
+There are three ways to generate point clouds: "2D-3", "2D" and "3D". 
+They can be chosen with the parameter "MD_Method".
+The input is composed of the number of grid points in each layer, "NGrid",
+the number of Necks in each layer, "NNecks", the number of layers, "NLayers",
+and the size of the catenoidal necks in the output, "NeckSize".
+WARNING don't choose NeckSize too large to avoid overlap.
+=#
+function generateGridLayers(NGrid::Int, NNecks::Int, NLayers::Int, NeckSize::Int; energy = "lennardjones", MD_Method = "2D-3")
+    NLayers%2==0 || throw(error("There must be an even number of layers!"))
+    MD_Method=="2D-3" || MD_Method=="2D" || MD_Method=="3D" || throw(error("Please choose a permissible MD Method!"))
+    energy == "riesz" || energy == "lennardjones" || throw(error("Please choose a permissible potential!"))
+    (MD_Method=="2D-3" && NLayers>=4) || throw(error("When using the method 2D-3 you need to use at least 4 layers!"))
+
+    pointGrid = [[Point3f0(a,b,c) for a in 1/(NGrid*2):1/NGrid:1-1/(NGrid*2) for b in 1/(NGrid*2):1/NGrid:1-1/(NGrid*2)]  for c in 0:1/NLayers:1-1/NLayers]
+    xvarz = [Variable(:x, layer, neck, pos) for layer in 1:NLayers for neck in 1:NNecks for pos in 1:2]
+    gridPositions = [[(i,j) for i in 1:NGrid for j in 1:NGrid] for _ in 1:NLayers]
+    initialPoint = []
+    for layer in 1:NLayers
+        for _ in 1:NNecks
+            randPos = rand(gridPositions[layer])
+            gridPositions[layer] = filter!(t->t!=(randPos), gridPositions[layer])
+            gridPositions[layer!=NLayers ? layer+1 : 1] = filter!(t->t!=(randPos), gridPositions[layer!=NLayers ? layer+1 : 1])
+            gridPositions[layer!=1 ? layer-1 : NLayers] = filter!(t->t!=(randPos), gridPositions[layer!=1 ? layer-1 : NLayers])
+            append!(initialPoint, Vector{Float64}(pointGrid[layer][(randPos[1]-1)*NGrid+(randPos[2]-1)%NGrid+1][1:2]))
+        end
+    end
+
+    xs, periodic_xs = create_Periods(NLayers, NNecks; MD_Method = MD_Method)
+    neckConfig = gradientDescent(periodic_xs, initialPoint, xvarz, NGrid; energy = energy, MD_Method = MD_Method)
+
+    totalGrid = Array{Any,4}(undef, NLayers, NGrid, NGrid, 3)
+    layerColours = Array{Any,3}(undef, NLayers, NGrid, NGrid)
+    for layer in 1:NLayers, pos1 in 0:NGrid-1, pos2 in 0:NGrid-1
+        totalGrid[layer, pos1+1, pos2+1,:] = [1/(NGrid*2)+pos1/NGrid, 1/(NGrid*2)+pos2/NGrid, layer/NLayers]
+        layerColours[layer, pos1+1, pos2+1] = mod(layer, 2)
+    end
+
+    scene2 = Scene()
+    cam3d!(scene2)
+    layerColours = color_Neck_Positions(neckConfig, xvarz, xs, totalGrid, layerColours, NeckSize; MD_Method = MD_Method)
+    open("pomelocoordinates.txt", "w") do io
+        for k in [(layer,pos1,pos2) for pos1 in 1:NGrid for pos2 in 1:NGrid for layer in 1:NLayers]
+            write(io, string(totalGrid[k[1],k[2],k[3],1], " ", totalGrid[k[1],k[2],k[3],2], " ", totalGrid[k[1],k[2],k[3],3], " ", layerColours[k[1],k[2],k[3]] == 1 ? "1" : "2", "\n"))
+        end
+    end;
+    foreach(k -> scatter!(scene2, Point3f0(totalGrid[k[1],k[2],k[3],:]), color=layerColours[k[1],k[2],k[3]] == 1 ? :red : :blue), [(layer,pos1,pos2) for pos1 in 1:NGrid for pos2 in 1:NGrid for layer in 1:NLayers ])
+    display(scene2)
+end
+
 end
