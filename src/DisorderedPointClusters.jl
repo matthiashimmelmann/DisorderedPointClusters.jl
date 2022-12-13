2400cd96f70c50ded42be2ee58551de00f5f712f
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2022-12-13 15:59:06 +0100

    Monte Carlo implemented

--- src/DisorderedPointClusters.jl
+++ src/DisorderedPointClusters.jl
@@ -4,8 +4,8 @@ module DisorderedPointClusters
 import GLMakie: scatter!, Axis3, Figure, Point3f0, Scene, cam3d!
 import LinearAlgebra: norm, det, pinv, svd
 import HomotopyContinuation: Variable, Expression, evaluate, differentiate
-import Random: shuffle
 import Combinatorics: multiexponents
+import ForwardDiff: gradient
 
 export generateGridLayers
 
@@ -31,17 +31,18 @@ function color_Neck_Positions(neckConfig, xvarz, xs, totalGrid, layerColours, ne
     return(layerColours)
 end
 
-function create_list_of_relevant_distances(periodic_xs; MD_Method)
+function create_list_of_relevant_distances(xs; MD_Method)
   list_of_relevant_molecule_interactions = []
-  for layer1 in 1:size(periodic_xs)[1], layer2 in 1:size(periodic_xs)[1], pos_torus in Int(4*size(periodic_xs)[2]/9)+1:Int(5*size(periodic_xs)[2]/9), pos_general in 1:size(periodic_xs)[2]
-      if MD_Method == "2D-3" && (mod(pos_torus, Int(size(periodic_xs)[2]/9))!=mod(pos_general,Int(size(periodic_xs)[2]/9)) && all(t->(layer1!=t[1]||pos_torus!=t[4]||t[3]!=pos_general) && (layer1!=t[1]||pos_torus!=t[3]||t[4]!=pos_general), list_of_relevant_molecule_interactions))
-          push!(list_of_relevant_molecule_interactions, (layer1, layer1, pos_torus, pos_general))
-      elseif (MD_Method == "2D" || MD_Method == "3D")  && (mod(pos_torus, Int(size(periodic_xs)[2]/9))!=mod(pos_general,Int(size(periodic_xs)[2]/9)) || layer1!=layer2)
-          push!(list_of_relevant_molecule_interactions, (layer1, layer2, pos_torus, pos_general))
+  for layer1 in 1:size(xs)[1], layer2 in layer1:size(xs)[1], pos_torus1 in 1:size(xs)[2], pos_torus2 in 1:size(xs)[2]
+      if MD_Method == "2D-3" && pos_torus1<pos_torus2 && all(t->(layer1!=t[1]||pos_torus1!=t[4]||t[3]!=pos_torus2) && (layer1!=t[1]||pos_torus1!=t[3]||t[4]!=pos_torus2), list_of_relevant_molecule_interactions)
+          push!(list_of_relevant_molecule_interactions, (layer1, layer1, pos_torus1, pos_torus2))
+      elseif (MD_Method == "2D" || MD_Method == "3D") && (layer1!=layer2 || pos_torus1<pos_torus2)
+          push!(list_of_relevant_molecule_interactions, (layer1, layer2, pos_torus1, pos_torus2))
       end
   end
 
-  return [sum((periodic_xs[layer1,pos1,:] - periodic_xs[layer2,pos2,:]).^2) for (layer1, layer2, pos1, pos2) in list_of_relevant_molecule_interactions]
+  multiPeriods = MD_Method == "3D" ? [[mod(i-1,3)-1, mod(div(i-1,3),3)-1,0] for i in 1:9] : [[mod(i-1,3)-1, mod(div(i-1,3),3)-1] for i in 1:9]
+  return [[sum((period + xs[layer1,pos1,:] - xs[layer2,pos2,:]).^2) for period in multiPeriods] for (layer1, layer2, pos1, pos2) in list_of_relevant_molecule_interactions]
 end
 
 function create_Periods(NLayers, NNecks; MD_Method)
@@ -57,7 +58,7 @@ function create_Periods(NLayers, NNecks; MD_Method)
           xs[layer,:,3] .= layer/NLayers
         end
     end
-    
+    #=
     periodic_xs = MD_Method == "3D" ? Array{Expression,3}(undef, NLayers, size(xs)[2]*9, 3) : Array{Expression,3}(undef, NLayers, size(xs)[2]*9, 2)
     #NOTE left to right 1-2-3, bottom to top 1-4-7
     for torus in 1:9
@@ -65,20 +66,51 @@ function create_Periods(NLayers, NNecks; MD_Method)
         periodic_xs[:, (torus-1)*size(xs)[2]+1:torus*size(xs)[2], 1] .+= (torus-1)%3-1
         periodic_xs[:, (torus-1)*size(xs)[2]+1:torus*size(xs)[2], 2] .+= Int(floor((torus-1)/3))-1
     end
+    =#
 
-    return xs, periodic_xs
+    return xs#, periodic_xs
 end
 
-function gradientDescent(periodic_xs, initialPoint, variables, NGrid; energy, MD_Method, maxIter)
-    #TODO take several random start points and do 1100 steps or so?
+function takeMonteCarloStep(sol, NGrid)
+  sol = sol + [rand([-1,0,1])/NGrid for _ in sol]
+  return sol - floor.(sol)
+end
+
+function monteCarlo(xs, initialPoints, xvarz, NGrid; energy, MD_Method, maxIter)
+  distanceList = create_list_of_relevant_distances(xs; MD_Method = MD_Method)
+  energyfunction = sol->sum(1 ./ (map(t->minimum(evaluate(t, xvarz=>sol)), distanceList).^2))
+  outputList = []
+  for initialPoint in initialPoints
+    push!(outputList, initialPoint)
+    prevsol = Base.copy(initialPoint)
+    #display(gradient(energyfunction,prevsol))
+    saveEnergy = energyfunction(prevsol)
+    for iter in 1:maxIter
+      cursol = takeMonteCarloStep(prevsol, NGrid)
+      energy = energyfunction(cursol)
+      iter%100==0 && println(iter," ", energy)
+      if energy < saveEnergy
+        outputList[end] = cursol
+        saveEnergy = energy
+        prevsol = cursol
+      end
+    end
+  end
+
+  return argmin(sol->energyfunction(sol), outputList)
+end
+
+
+#=
+function gradientDescent(xs, initialPoint, variables, NGrid; energy, MD_Method, maxIter)
     cursol = Base.copy(initialPoint)
 
-    distances = create_list_of_relevant_distances(periodic_xs; MD_Method = MD_Method)
+    distances = create_list_of_relevant_distances(xs; MD_Method = MD_Method)
     Q=0;
     if energy == "riesz"
         Q = sum([1/d^2 for d in distances])
     elseif energy == "lennardjones"
-        σ1 = MD_Method=="2D" ? sqrt(2)/(sqrt(size(periodic_xs)[1]*size(periodic_xs)[2]/9)) : sqrt(2)/(sqrt(size(periodic_xs)[2]/9))
+        σ1 = MD_Method=="2D" ? sqrt(2)/(sqrt(size(xs)[1]*size(xs)[2]/9)) : sqrt(2)/(sqrt(size(xs)[2]/9))
         Q = sum(((σ1/d)^6-(σ1/d)^3 for d in distances))
     end
     ∇Q = differentiate(Q, variables)
@@ -89,7 +121,7 @@ function gradientDescent(periodic_xs, initialPoint, variables, NGrid; energy, MD
     solutionarray = []
 
     for iter in 1:maxIter
-        println(iter, "\tMinima found: ", isNoMin,"\t stepsize: ", round(α, digits=3), "\t\t gradient norm: ", round(norm(evaluate(∇Q, variables=>cursol)), digits=2), "\t\t energy: ", round(evaluate(Q, variables=>cursol), digits=2))
+        println(iter, "\tMinima found: ", isNoMin,"\t stepsize: ", round(α, digits=3), "\t\t gradient norm: ", round(norm(evaluate(∇Q, variables=>cursol)), digits=1), "\t\t energy: ", round(evaluate(Q, variables=>cursol), digits=1))
         stepdirection = pinv(evaluate.(HessQ, variables=>cursol))*evaluate(∇Q, variables=>cursol)
         cursol = prevsol - α*stepdirection
         α = iter%80 == 0 ? 0.5 : α
@@ -123,6 +155,7 @@ function gradientDescent(periodic_xs, initialPoint, variables, NGrid; energy, MD
 
     return cursol
 end
+=#
 
 #=
 There are three ways to generate point clouds: "2D-3", "2D" and "3D". 
@@ -133,28 +166,31 @@ and the size of the catenoidal necks in the output, "NeckSize".
 WARNING don't choose NeckSize too large to avoid overlap.
 The maxIter parameter denotes how many MD-steps are supposed to be performed.
 =#
-function generateGridLayers(NGrid::Int, NNecks::Int, NLayers::Int, NeckSize::Int; energy = "lennardjones", MD_Method = "2D-3", maxIter = 2100)
+function generateGridLayers(NGrid::Int, NNecks::Int, NLayers::Int, NeckSize::Int; energy = "lennardjones", MD_Method = "2D-3", maxIter = 25000, monteCarloStartPoints = 10)
     NLayers%2==0 || throw(error("There must be an even number of layers!"))
     MD_Method=="2D-3" || MD_Method=="2D" || MD_Method=="3D" || throw(error("Please choose a permissible MD Method!"))
     energy == "riesz" || energy == "lennardjones" || throw(error("Please choose a permissible potential!"))
-    (MD_Method=="2D-3" && NLayers>=4) || throw(error("When using the method 2D-3 you need to use at least 4 layers!"))
+    (MD_Method!="2D-3" || NLayers>=4) || throw(error("When using the method 2D-3 you need to use at least 4 layers!"))
 
     pointGrid = [[Point3f0(a,b,c) for a in 1/(NGrid*2):1/NGrid:1-1/(NGrid*2) for b in 1/(NGrid*2):1/NGrid:1-1/(NGrid*2)]  for c in 0:1/NLayers:1-1/NLayers]
     xvarz = [Variable(:x, layer, neck, pos) for layer in 1:NLayers for neck in 1:NNecks for pos in 1:2]
     gridPositions = [[(i,j) for i in 1:NGrid for j in 1:NGrid] for _ in 1:NLayers]
-    initialPoint = []
-    for layer in 1:NLayers
-        for _ in 1:NNecks
-            randPos = rand(gridPositions[layer])
-            gridPositions[layer] = filter!(t->t!=(randPos), gridPositions[layer])
-            gridPositions[layer!=NLayers ? layer+1 : 1] = filter!(t->t!=(randPos), gridPositions[layer!=NLayers ? layer+1 : 1])
-            gridPositions[layer!=1 ? layer-1 : NLayers] = filter!(t->t!=(randPos), gridPositions[layer!=1 ? layer-1 : NLayers])
-            append!(initialPoint, Vector{Float64}(pointGrid[layer][(randPos[1]-1)*NGrid+(randPos[2]-1)%NGrid+1][1:2]))
-        end
+    initialPoints = []
+    for _ in 1:monteCarloStartPoints
+      initialPoint = []
+      for layer in 1:NLayers, _ in 1:NNecks
+        randPos = rand(gridPositions[layer])
+        gridPositions[layer] = filter!(t->t!=(randPos), gridPositions[layer])
+        gridPositions[layer!=NLayers ? layer+1 : 1] = filter!(t->t!=(randPos), gridPositions[layer!=NLayers ? layer+1 : 1])
+        gridPositions[layer!=1 ? layer-1 : NLayers] = filter!(t->t!=(randPos), gridPositions[layer!=1 ? layer-1 : NLayers])
+        append!(initialPoint, Vector{Float64}(pointGrid[layer][(randPos[1]-1)*NGrid+(randPos[2]-1)%NGrid+1][1:2]))
+      end
+      push!(initialPoints, initialPoint)
     end
 
-    xs, periodic_xs = create_Periods(NLayers, NNecks; MD_Method = MD_Method)
-    neckConfig = gradientDescent(periodic_xs, initialPoint, xvarz, NGrid; energy = energy, MD_Method = MD_Method, maxIter = maxIter)
+    xs#=, periodic_xs=# = create_Periods(NLayers, NNecks; MD_Method = MD_Method)
+    #neckConfig = gradientDescent(periodic_xs, initialPoint, xvarz, NGrid; energy = energy, MD_Method = MD_Method, maxIter = maxIter)
+    neckConfig = monteCarlo(xs, initialPoints, xvarz, NGrid; energy = energy, MD_Method = MD_Method, maxIter = maxIter)
 
     totalGrid = Array{Any,4}(undef, NLayers, NGrid, NGrid, 3)
     layerColours = Array{Any,3}(undef, NLayers, NGrid, NGrid)
